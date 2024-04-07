a61a9038c4e398ca755299074f27a201900e99fb
Setare Maleki <72472685+setaremalekiii@users.noreply.github.com>
2024-04-07 11:12:03 -0700

    final

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -23,17 +23,18 @@ function Homepage() {
 
   return (
 <div style={{ 
-  backgroundImage: `url(${backgroundImage})`, 
-  backgroundSize: 'cover', // Zoom out the background image slightly
+  backgroundImage: `url(${backgroundImage})`,
+  backgroundSize: '90%', // Scales the image as large as possible without cropping or stretching the image
   backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  minHeight: '100vh', // Ensure the background covers the entire viewport
+  backgroundPosition: 'center center', // Centers the image within the container
+  width: '100vw', // Full viewport width
+  height: '100vh', // Full viewport height
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'center',
   alignItems: 'center',
   color: 'white',
-  position: 'relative', // Ensure that absolute positioning works relative to this container
+  position: 'relative', // Ensures that absolute positioning is relative to this container
 }}>
       {/* Centered GIF */}
       <img src={gifImage} alt="GIF" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: '300px', height: '300px' }} />
@@ -42,7 +43,7 @@ function Homepage() {
 <Link to="/detect">
   <button 
     style={{ 
-      padding: '10px 60px', // Adjust padding to make it shorter
+      padding: '10px 100px', // Adjust padding to make it shorter
       fontSize: '16px', 
       backgroundColor: 'transparent', // Set background color to transparent
       color: 'transparent', // Set text color to transparent
@@ -50,16 +51,38 @@ function Homepage() {
       borderRadius: '5px', 
       cursor: 'pointer',
       position: 'absolute',
-      bottom: '20px',
+      bottom: '50px',
       left: '50%', // Center the button horizontally
       transform: 'translateX(-50%)', // Center the button horizontally
     }}
   >
   </button>
 </Link>
-//<Link to="/detect"><button>Go to Detection</button></Link>
+
+<Link to="/profile">
+  <button 
+    style={{ 
+      padding: '50px 60px', // Adjust padding as needed
+      fontSize: '16px',
+      backgroundColor: 'transparent', // Button background color
+      color: 'transparent', // Button text color
+      border: 'none',
+      borderRadius: '5px',
+      cursor: 'pointer',
+      position: 'absolute',
+      bottom: '800px', // Increase to move the button up
+      left: 'calc(50% + 550px)', // Add to the 50% to move the button to the right
+      // Remove the transform property if you are not centering
+    }}
+  >
+  </button>
+</Link>
+
     </div>
+    
   );
 }
 
 export default Homepage;
+
+//<Link to="/detect"><button>Go to Detection</button></Link>
