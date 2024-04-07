d83db5c6608ddfd6dd23b7ab4ffce8c88f062324
Setare Maleki <72472685+setaremalekiii@users.noreply.github.com>
2024-04-07 11:13:46 -0700

    final

--- src/components/Smoothie.js
+++ src/components/Smoothie.js
@@ -14,16 +14,16 @@ function Smoothie() {
       <Link to="https://www.floatingkitchen.net/apple-pie-smoothie-bowl/" target="_blank" rel="noopener noreferrer">
         <button 
           style={{ 
-            padding: '10px 110px', // Adjust padding to make it shorter
+            padding: '50px 110px', // Adjust padding to make it shorter
             fontSize: '16px', 
-            backgroundColor: '#ff4500', // Orange color for better visibility
+            backgroundColor: 'transparent', // Orange color for better visibility
             color: 'transparent', // White text color
             border: 'none', 
             borderRadius: '30px', 
             cursor: 'pointer',
             position: 'absolute',
             bottom: '100px',
-            left: '80%', // Move the button slightly to the left
+            left: '45%', // Move the button slightly to the left
             transform: 'translateX(-50%)', // Center the button horizontally
           }}
         >
