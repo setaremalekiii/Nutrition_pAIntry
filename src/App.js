54cec975dd80fc2e053e2b3741482ff3d3d843ee
Nikoo Ghasemkhanvali <133624721+nikoov@users.noreply.github.com>
2024-04-07 08:09:27 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Homepage from './components/Homepage';
-import Scanning from './components/Scanning';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Homepage from './components/Homepage';
+import Hotdog from './components/Hotdog'; // Correct import statement
+import Smoothie from './components/Smoothie';
+import Profile from './components/Profile';
+
+import Scanning from './components/Scanning';
 
 function App() {
   return (
@@ -10,6 +14,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} exact />
         <Route path="/detect" element={<Scanning />} />
+        <Route path="/first" element={<Hotdog />} />
+        <Route path="/second" element={<Smoothie />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
