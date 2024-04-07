3bfdfe848f8c947f8c378bf3ae0dbdd7f8c2f78e
Setare Maleki <72472685+setaremalekiii@users.noreply.github.com>
2024-04-07 08:27:28 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/Homepage';
-import Hotdog from './components/Hotdog'; // Correct import statement
-import Smoothie from './components/Smoothie';
-import Profile from './components/Profile';
-
 import Scanning from './components/Scanning';
+import Hotdog from './components/Hotdog';
+import Smoothie from './components/Smoothie';
+
+import './App.css';
 
 function App() {
   return (
@@ -14,9 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} exact />
         <Route path="/detect" element={<Scanning />} />
-        <Route path="/first" element={<Hotdog />} />
-        <Route path="/second" element={<Smoothie />} />
-        <Route path="/profile" element={<Profile />} />
+       <Route path="/second" element={<Smoothie />} /> {/* Page A component */}
+       <Route path="/first" element={<Hotdog />} /> {/* Page B component */}
       </Routes>
     </Router>
   );
